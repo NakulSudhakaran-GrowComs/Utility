efce9b7cb6e442fd6c909eebbc209df2acc9a4b6
GrowComs-Tester <it@growcoms.com>
2024-06-12 23:08:17 +0530

    Delete agrilin directory

--- agrilin/src/test/java/com/agrilin/locators/CreateAccountLocators.java
+++
@@ -1,7 +0,0 @@
-package com.agrilin.locators;
-
-public class CreateAccountLocators {
-
-	public static final String ENTER_FULL_NAME_XPATH="//div/input[@name='first_name']";
-	//public static final String=""
-}
--- agrilin/src/test/resources/locators/XpathLocators.java
+++
@@ -1,7 +0,0 @@
-
-//import utilities.AutomationLibrary;
-public class XpathLocators {
-	
-	public static final String="//div//img[@class='block w-full']";
-
-}
